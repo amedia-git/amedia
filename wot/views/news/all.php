bf4fe43bf98b59b5574808b1088b5d830625bbd5
Andrius <andrius@amedia.lt>
2015-03-22 21:49:45 +0200

    Views direktorija

---
+++ wot/views/news/all.php
@@ -0,0 +1,41 @@
+<?php
+use yii\helpers\Html;
+//$this->title = 'Contact';
+//$this->params['breadcrumbs'][] = $this->title;
+//$this->params['breadcrumbs'][] = 'All';
+//$this->params['breadcrumbs'][] = 'Vote';
+?>
+<h1><?= Html::encode($this->title) ?></h1>
+<section>
+    <article>
+        <h3 class="title">First article</h3>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec ornare quam, ut pretium tellus. Cras sit amet mi egestas, lobortis neque id, pharetra turpis. Curabitur eros dui, elementum eu accumsan rutrum, placerat id ante. Nunc gravida tempor orci, vel consectetur leo ullamcorper a. Etiam pretium mollis metus, in pellentesque neque consectetur sed. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus finibus tincidunt turpis eu porttitor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed aliquam est a ullamcorper lacinia. Praesent id tempus erat. Phasellus a interdum nunc, id tincidunt nulla.
+        </p>
+    </article>
+
+    <article>
+        <h3 class="title">Second article</h3>
+        <p>
+            Nunc pretium, dolor sed pellentesque mollis, sem urna maximus ante, et sollicitudin massa dui at ligula. Duis pretium feugiat leo id ornare. Nullam efficitur rhoncus nunc, eu hendrerit mi pellentesque vitae. In imperdiet, urna id tristique tempor, dui urna tempor dui, eu pretium ligula purus eget ante. Nulla semper pretium arcu at faucibus. Sed eleifend auctor orci vitae luctus. Curabitur tempor tortor ut vulputate euismod. Ut pulvinar mi elit, in auctor orci pretium eu. Vivamus vulputate tristique felis eget malesuada. Nam laoreet, felis in blandit vestibulum, urna nibh scelerisque sapien, et hendrerit augue erat et magna. Etiam iaculis metus non purus mollis, vitae interdum tortor tincidunt. Etiam interdum, nunc ac finibus sagittis, libero mi gravida leo, non blandit leo purus nec nisi. Nulla efficitur eu sem et viverra. Ut ut magna eu mi lacinia mattis. Mauris placerat, metus at feugiat consectetur, nunc odio laoreet leo, a pharetra ligula velit vitae ex.
+        </p>
+    </article>
+    <article>
+        <h3 class="title">Third article</h3>
+        <p>
+            Quisque tincidunt quis neque in varius. Donec vehicula bibendum ipsum non volutpat. Curabitur turpis sapien, molestie at risus vel, congue rutrum sem. Vestibulum vel ante porttitor, aliquam lacus at, posuere tortor. Donec sed eros fringilla, maximus nisl in, dignissim lectus. Ut ut ullamcorper lorem. Donec sollicitudin dolor non lectus iaculis volutpat. Donec porta neque ut gravida ultrices. Ut sagittis id elit sed aliquam. Donec eget placerat erat, non tempor justo. Ut ac urna id erat maximus fermentum nec vel nibh. Nulla imperdiet nulla ac urna accumsan euismod. Vivamus dignissim non tortor vel molestie.
+        </p>
+    </article>
+    <article>
+        <h3 class="title">Fourth article</h3>
+        <p>
+            Maecenas eu arcu semper, eleifend tortor id, tempus augue. Vivamus accumsan velit at magna ultrices efficitur. Donec leo ante, lacinia id aliquet quis, hendrerit ac augue. Aenean suscipit quis lectus nec placerat. Nam ut condimentum sapien. Vestibulum lobortis commodo ante, at tincidunt augue elementum quis. Etiam suscipit, enim sed vulputate pulvinar, odio odio accumsan ex, sed pellentesque mi ligula eget mauris. Vestibulum vitae odio est. Nulla suscipit diam eget tempus pretium. Duis maximus lobortis convallis. Aenean ut dolor velit. Ut luctus sagittis tincidunt.
+        </p>
+    </article>
+    <article>
+        <h3 class="title">Fifth article</h3>
+        <p>
+            Aliquam erat volutpat. Fusce imperdiet elit vitae tellus commodo, id hendrerit tortor pretium. Integer a nulla ut nulla eleifend ornare. Nam sem lacus, aliquam eget ipsum id, mattis interdum turpis. Aliquam vitae odio arcu. Cras felis purus, rhoncus eu commodo id, tempus et enim. Duis sed turpis vel tellus pellentesque posuere. Suspendisse nec purus vel neque ullamcorper interdum. Integer eget libero eget arcu porta aliquam. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
+        </p>
+    </article>
+</section>
